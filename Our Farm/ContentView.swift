0661c223ee9790e34a173bdc03e63f71bf5a4255
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2024-07-30 12:51:22 +0800

    Initial Commit

---
+++ Our Farm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Our Farm
+//
+//  Created by Hananiah on 2024/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Our Farm/Our_FarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Our_FarmApp.swift
+//  Our Farm
+//
+//  Created by Hananiah on 2024/7/30.
+//
+
+import SwiftUI
+
+@main
+struct Our_FarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
